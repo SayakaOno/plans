98ac6073f0b224b6c92bc6e0e1ca8415fd61c8ef
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-06-14 20:48:04 -0700

    implement tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import moment from 'moment';
-import { Layout, Row, Button, DatePicker } from 'antd';
+import { Layout, Tabs, Row, Button, DatePicker } from 'antd';
 import MainTarget from './MainTarget';
 import ToDo from './ToDo';
+import GenreDetail from './GenreDetail';
 import './App.css';
 
 const App = () => {
   const { Content } = Layout;
+  const { TabPane } = Tabs;
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Content style={{ padding: 50 }}>
-        <MainTarget />
-        <ToDo />
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="TODO" key="1">
+            <ToDo />
+          </TabPane>
+          <TabPane tab="Progress" key="2">
+            <MainTarget />
+            <GenreDetail />
+          </TabPane>
+          <TabPane tab="Settings" key="3">
+            Settings
+          </TabPane>
+        </Tabs>
       </Content>
     </Layout>
   );
